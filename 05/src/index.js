486a06e7cc21ca6f4b7c2d85e3113efb40c3a520
Tatsuya Nagase <tatsuya.nagase@nts.ricoh.co.jp>
2015-07-01 11:27:29 +0900

    0701 AM

--- 05/src/index.js
+++ 05/src/index.js
@@ -26,49 +26,52 @@ app.use(session({
 
 // ログイン画面
 app.get('/login', function(req, res) {
-  return res.status(200).render('login', {errorMessages: []});
+  res.status(200).render('login', {errorMessages: null});
 });
 
 // ユーザ登録画面
 app.get('/registration', function(req, res) {
-  res.status(200).render('registration', {errorMessages: []});
+  res.status(200).render('registration', {errorMessages: null});
 });
 
-// ユーザ登録画面
+// 
 app.get('/illegal_access', function(req, res) {
   res.status(200).render('illegal_access', {errorMessages: ['ログインして下さい。']});
 });
 
 // 成功画面
 app.get('/main', function(req, res) {
-  if (req.session.username) {
-    db.connect().then(function() {
-      return db.getAllUser();
-    }).then(function(resultArr) {
-      var loginUser = req.session.username;
-      res.status(200).render('main', {
-        users: resultArr,
-        loginUser: loginUser
-      });
-    });
-    return;
+  if (!req.session) {
+    return res.status(302).redirect('/illegal_access');
   }
-  return res.status(302).redirect('/illegal_access');
+  if (!req.session.username) {
+    return res.status(302).redirect('/illegal_access');
+  }
+  db.connect().then(function() {
+    return db.getAllUser();
+  }).then(function(resultArr) {
+    var loginUser = req.session.username;
+    res.status(200).render('main', {
+      users: resultArr,
+      loginUser: loginUser
+    });
+  });
 });
 
 // 登録ユーザ情報表示画面
 app.get('/user', function(req, res) {
-  if (req.session.username) {
-    var username = req.query.username;
-    var loginUser = req.session.username;
-    res.status(200).render('user', {
-      loginUser: loginUser,
-      username: username
-    });
-    return;
+  if (!req.session) {
+    return res.status(302).redirect('/illegal_access');
   }
-  console.log('statusCode : ' + req.statusCode);
-  return res.status(302).redirect('/illegal_access');
+  if (!req.session.username) {
+    return res.status(302).redirect('/illegal_access');
+  }
+  var username = req.query.username;
+  var loginUser = req.session.username;
+  res.status(200).render('user', {
+    loginUser: loginUser,
+    username: username
+  });
 });
 
 // ログイン処理
@@ -88,9 +91,9 @@ app.post('/login', function(req, res) {
       return res.status(400).render('login', { errorMessages: ['passwordが違います。'] });
     }
     req.session.username = username;
-    return res.status(200).redirect('/main');
+    return res.redirect('/main');
   }).catch(function(err) {
-    return res.status(500).render('login', { errorMessages: [err] });
+    return res.status(500).render('login', { errorMessages: ['ログイン失敗です'] });
   });
 });
 
@@ -115,11 +118,10 @@ app.post('/registration', function(req, res) {
     return db.register(username, password);
   }).then(function() {
     req.session.username = username;
-    req.session.flg = true;
     return res.status(200).redirect('main');
   }).catch(function(err) {  // save()に失敗した場合と、すでに登録済みのusernameを登録しようとした場合がある
     //return res.status(400).render('registration', { errors: [err] });
-    return res.status(400).render('registration', { errorMessages: ['登録失敗だす'] });
+    return res.status(400).render('registration', { errorMessages: ['登録失敗です'] });
   });
 });
 
